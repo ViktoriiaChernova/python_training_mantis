2eacda51310706ea135360367dce44a2e1a17acc
v.chernova <wecan-doit@yandex.ru>
2024-11-06 23:12:31 +0300

    test_signup_new_account

--- fixture/application.py
+++ fixture/application.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
 from fixture.james import JamesHelper
+from fixture.mail import MailHelper
+from fixture.signup import SignupHelper
 
 class Application:
     def __init__(self, browser, config):
@@ -17,6 +19,8 @@ class Application:
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
         self.james = JamesHelper(self)
+        self.mail = MailHelper(self)
+        self.signup = SignupHelper(self)
         self.config = config
         self.base_url = config['web']['baseUrl']
 
